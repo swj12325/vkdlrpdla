6ee337df644277f5b481cf42bf36f4af47ffadc7
swj12325 <dkakxpfktm60@naver.com>
2021-10-02 17:55:51 +0900

    game01

--- pygame1/pygame1.py
+++ pygame1/pygame1.py
@@ -8,8 +8,15 @@ pygame.display.set_caption("pygame")
 fps = pygame.time.Clock()
 
 play = True
-create = False
+create = True
+delete = False
+start = True
 move = 600
+
+red = (255,0,0)
+black = (0,0,0)
+color = [red, black]
+count = 0
 
 while play:
     deltaTime = fps.tick(60)
@@ -17,18 +24,29 @@ while play:
     pygame.draw.circle(background, (255,255,255), (120,180), 30)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            play = False            
-    
-    if event.type == pygame.MOUSEBUTTONDOWN:
-        create = True
-
-    if create:   
-        if move >= 0:
-            pygame.draw.circle(background, (255,0,0), ((move,180)), 30)
-            move -= 5
-            pygame.display.update()
-        else:
+            play = False               
+        
+        if event.type == pygame.KEYDOWN:
+            if (move > 140 and move < 180) or (move < 100 and move > 60):
+                print("miss")
+            elif move < 140 and move > 100:
+                print("good") 
             create = False
+            move = 600
+            
+        
+        if event.type == pygame.KEYUP:
+            create = True
+        
+    if create: 
+        if move >= 0:
+            count = 0
+            pygame.draw.circle(background, color[count], ((move,180)), 30)
+            move -= 5
+        else:
+            move = 600
+            create = False
+    pygame.display.update()
 
 
 pygame.quit()
--- pygame_image/game01.py
+++ pygame_image/game01.py
@@ -2,19 +2,26 @@ import pygame
 
 pygame.init()
 
-background = pygame.display.set_mode((480, 360))
-pygame.display.set_caption("pygame")
-
 image_bg = pygame.image.load("image/qorud.svg")
 image_fish = pygame.image.load("image/fish.svg")
 
-size_bg_width = background.get_size()[0]
-size_bg_height = background.get_size()[1]
+
+# list = [0,1,2,3]
+# list[0] => 0
+# list[3] => 3
+# "list" == "background.get_size()"
+size_bg_width = image_bg.get_rect().size[0]
+size_bg_height = image_bg.get_rect().size[1]
+print(size_bg_height,size_bg_width)
+
+background = pygame.display.set_mode((size_bg_width, size_bg_height))
+pygame.display.set_caption("pygame")
+
 
 size_fish_width = image_fish.get_rect().size[0]
 size_fish_height = image_fish.get_rect().size[1]
 
-x_pos_fish = size_bg_width/2 - size_fish_width/2
+x_pos_fish = size_bg_width/2
 y_pos_fish = 180
 
 play = True
@@ -23,7 +30,7 @@ while play:
         if event.type == pygame.QUIT:
             play = False
     
-    background.blit(image_bg, (-9,-7))
+    background.blit(image_bg, (0,0))
     background.blit(image_fish, (x_pos_fish, y_pos_fish))
     pygame.display.update()
 
